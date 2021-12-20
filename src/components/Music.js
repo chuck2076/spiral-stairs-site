a36b19c9e2ceba04250f215489b3a32964f10bbc
Mat Lundin <matmll12@gmail.com>
2021-12-20 16:55:33 -0600

    fixed spotify embed on listen component

--- src/components/Music.js
+++ src/components/Music.js
@@ -11,7 +11,8 @@ export default function Music(){
               <h3>{album.artist}</h3>
               <h4>{album.title}</h4>
               <img src={album.image} alt={album.title}></img>
-              <iframe src="https://open.spotify.com/embed/album/1aoq6sALy90x5gpPEqy6ag?utm_source=generator" width="100%" height="80" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+              <iframe title={album.title} src={album.spotEmbed} width="100%" height="80" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+            {/* {album.spotEmbed.replace(/^"(.*)"$/, '$1')} */}
             </div>
            )
         })}
