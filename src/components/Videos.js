5b3b02bad34469c7c870e112dcaeb0311a118d79
Mat Lundin <matmll12@gmail.com>
2021-12-21 15:33:17 -0600

    watch page displays artist youtubes now

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,20 +1,31 @@
 // Carousel of Videos from YouTube
-
 import React, { useState } from "react";
+import videoData from '../videos.json'
 
 export default function Videos(){
-    const [videos, setVideos] = useState('')
-    console.log('videos= '+videos)
+    const [artist, setArtist] = useState('')
+    console.log('videos= '+artist)
     return( <div>
         <h1>Watch Page</h1>
-        <h6>Bootstrap Select</h6>        
+        <h6>Choose an artist to view YouTube videos!</h6>        
         <select className="custom-select" id="artist"
-        onChange={(e)=>setVideos(e.target.value)}>
+        onChange={(e)=>setArtist(e.target.value)}>
           <option defaultValue>Choose Artist</option>
-          <option value="SpiralStairs">Spiral Stairs</option>
-          <option value="Preston">Preston School of Industry</option>
+          <option value="Spiral Stairs">Spiral Stairs</option>
+          <option value="Preston School of Industry">Preston School of Industry</option>
           <option value="Pavement">Pavement</option>
-        </select>   
+        </select>
+        <br/>
+        {videoData.map((video)=> {
+            if (video.artist === artist){
+                const srcUrl = "https://www.youtube.com/embed/"+video.url
+                return (
+                    <iframe key={video.url} width="560" height="315" src={srcUrl} title={video.title} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                )
+            } else {
+                return null
+            }
+        })}
     </div>
    )
 };
