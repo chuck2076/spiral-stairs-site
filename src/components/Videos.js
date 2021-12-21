7fee4951f55ba9651671563333ea1ffa0ee3745d
Mat Lundin <matmll12@gmail.com>
2021-12-21 15:18:08 -0600

    added select button which updates the state to be used for vid display

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,10 +1,20 @@
 // Carousel of Videos from YouTube
 
-import React from "react";
+import React, { useState } from "react";
 
 export default function Videos(){
+    const [videos, setVideos] = useState('')
+    console.log('videos= '+videos)
     return( <div>
         <h1>Watch Page</h1>
+        <h6>Bootstrap Select</h6>        
+        <select className="custom-select" id="artist"
+        onChange={(e)=>setVideos(e.target.value)}>
+          <option defaultValue>Choose Artist</option>
+          <option value="SpiralStairs">Spiral Stairs</option>
+          <option value="Preston">Preston School of Industry</option>
+          <option value="Pavement">Pavement</option>
+        </select>   
     </div>
    )
-}
+};
