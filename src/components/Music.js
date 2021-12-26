c8cba019a56c846a9e0bb0b07a4af9711acc7324
Chuck Stephens <chuckstephens2076@gmail.com>
2021-12-26 17:52:01 -0600

    got the carousel working, need to style better

--- src/components/Music.js
+++ src/components/Music.js
@@ -1,25 +1,30 @@
 // Carousel of Albums with Listen and Buy Links
 import React from "react";
 import musicData from '../music.json'
-import Carousel from 'react-bootstrap/Carousel';
+import RBCarousel from "react-bootstrap-carousel";
+import "react-bootstrap-carousel/dist/react-bootstrap-carousel.css";
 
 export default function Music(){
    return( <div>
         <h2>Listen Page</h2>
-         <Carousel>
-            <Carousel.Item>
+         <RBCarousel               
+            animation={true}
+            autoplay={false}
+              slideshowSpeed={2000}
+              defaultActiveIndex={0}
+              version={4}
+            >
         {musicData.map((album)=>{
            return (
             <div key = {album.title} className="d-block w-100">
               <h3>{album.artist}</h3>
               <h4>{album.title}</h4>
               <img src={album.image} alt={album.title}></img>
-              <iframe title={album.title} src={album.spotEmbed} width="20%" height="80" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+              <iframe title={album.title} src={album.spotEmbed} width="100%" height="80" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
             </div>
            )
         })}
-         </Carousel.Item>
-        </Carousel>
+         </RBCarousel>
     </div>
    )
 }
