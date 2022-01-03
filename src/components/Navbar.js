21e4a14dcaca41b256c6910d036c37498291671d
Mat Lundin <matmll12@gmail.com>
2022-01-03 16:21:34 -0600

    updated nav bar to use routing, external pages open in new tab

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,34 +6,34 @@ export default function Navbar({currentPage, setCurrentPage}){
     console.log("state = "+currentPage)
     return (
         <nav className="navbar navbar-expand-lg navbar-light">
- <a className="navbar-brand" href="#"><h1>Spinal Spiral Spirally</h1></a>
+ <a className="navbar-brand" href="/"><h1>Spinal Spiral Spirally</h1></a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
   <div className="collapse navbar-collapse" id="navbarNavDropdown">
   <h3>  <ul className="navbar-nav ml-5">
       <li id="home" className={currentPage === 'home' ? "nav-item active" : "nav-item"}>
-        <a className="nav-link" href="#"
+        <a className="nav-link" href="/"
         onClick={()=> setCurrentPage('home')}
         >Home <span className="sr-only">(current)</span></a>
       </li>
       <li id="tour" className={currentPage === 'tour' ? "nav-item active" : "nav-item"}>
-        <a className="nav-link" href="https://pavementband.com/#tour"
+        <a className="nav-link" href="https://pavementband.com/#tour" target="_blank"
         onClick={()=> setCurrentPage('tour')}
         >Tour</a>
       </li>
       <li id="music" className={currentPage === 'music' ? "nav-item active" : "nav-item"}>
-        <a className="nav-link" href="#"
+        <a className="nav-link" href="/listen"
         onClick={()=> setCurrentPage('music')}
         >Listen</a>
       </li>
       <li id="videos" className={currentPage === 'videos' ? "nav-item active" : "nav-item"}>
-        <a className="nav-link" href="#"
+        <a className="nav-link" href="/watch"
         onClick={()=> setCurrentPage('videos')}
         >Watch</a>
       </li>
       <li id="merch" className={currentPage === 'merch' ? "nav-item active" : "nav-item"}>
-        <a className="nav-link" href="https://kf-merch.com/collections/spiral-stairs"
+        <a className="nav-link" href="https://kf-merch.com/collections/spiral-stairs" target="_blank"
         onClick={()=> setCurrentPage('merch')}
         >Shop</a>
       </li>
