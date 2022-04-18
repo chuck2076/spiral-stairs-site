f6f510c1f7f99cd1c3e3f54a5b3ffbca96e42f26
Mat Lundin <matlundin.dev@gmail.com>
2022-04-18 16:26:34 -0500

    stylized dev credits added

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from "./components/Footer";
 import NewNavbar from "./components/Navbar";
 import Home from './components/Home';
-import Music from './pages/Music'
-import Tour from './pages/Tour'
-import Videos from './pages/Videos'
-import Merch from './pages/Merch'
+import Music from './pages/Music';
+import Tour from './pages/Tour';
+import Videos from './pages/Videos';
+import Merch from './pages/Merch';
 
+console.log(`%cDeveloped By`, '🐦;background: rgb(190 170 140); color: #444; padding: 3px; border-radius: 5px;');
+console.log(`     %cChuck Stevens: https://github.com/chuck2076`, '🐦;background: lightblue; color: #444; padding: 3px; border-radius: 5px;');
+console.log(`     %cMat Lundin: https://github.com/mat-lundin`, '🐦;background: lightblue; color: #444; padding: 3px; border-radius: 5px;');
 export default function App() {
-//  const [currentPage, setCurrentPage] = useState('home');
+
   return (
     <main>
       <BrowserRouter>
