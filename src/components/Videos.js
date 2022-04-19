0458ed384da6cfc5535050003efb6578b4e1d012
Chuck Stephens <chuckstephens2076@gmail.com>
2022-04-18 19:02:06 -0500

    fixing watch boxes

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -5,7 +5,7 @@ import videoData from '../videos.json'
 export default function Videos(){
     const [artist] = useState('')
     console.log('videos= '+artist)
-    return( <div className="xs-12 justify-content-center flex-content-center">
+    return( <div className="xs-12 ml-5 align-items-center flex-content-center">
         
         {/* <h6 style = {{color: "white"}}>Choose an artist to view YouTube videos!</h6>        
         <select className="custom-select m-50% w-50px" id="artist"
@@ -14,12 +14,12 @@ export default function Videos(){
           <option value="Preston School of Industry">Preston School of Industry</option>
           <option value="Pavement">Pavement</option>
         </select> */}
-        <p className="xs-12 justify-content-center flex-content-center">
+        <p className="xs-12 align-items-center flex-content-center flex direction-row">
         {videoData.map((video)=> {
   //          if (video.artist === artist){
                 const srcUrl = "https://www.youtube.com/embed/"+video.url
                 return (
-                    <iframe key={video.url} width="30%" height = "100%" style={{margin: '0.5%', borderImage: 'url(/images/Rainbow.jpg) 100', borderWidth: '3px', borderStyle: 'solid'}} src={srcUrl} title={video.title} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                    <iframe key={video.url} width="40%" height = "100%" style={{margin: '0.5%', borderImage: 'url(/images/Rainbow.jpg) 100', borderWidth: '3px', borderStyle: 'solid'}} src={srcUrl} title={video.title} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
                 )
     //        } else {
     //            return null
