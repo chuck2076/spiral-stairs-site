f42aaf893ae6f8c1fa24efbf7c4885da0fa74312
Chuck Stephens <chuckstephens2076@gmail.com>
2022-04-25 19:45:00 -0500

    added preorder button

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 //This is the latest news, album releases, and other updates.
 
+import { Button } from "bootstrap";
 import React from "react";
 import {Container, Row, Col} from 'react-bootstrap'
 
@@ -7,10 +8,11 @@ import {Container, Row, Col} from 'react-bootstrap'
 export default function Home(){
     return(
     <Container flex="true">
-        <Row>
-        <a className="justify-content-center" flex="true" href="https://spiralstairspsoi.bandcamp.com/">
-        <h3 className="cliptwo mt-5">New Album</h3><h1 className="clip">Medley Attack!!!</h1><h3 className="cliptwo">July 08 2022</h3>
-        </a>
+        <Row className="justify-content-center mt-5">
+        <a flex="true" href="https://spiralstairspsoi.bandcamp.com/">
+        <h3 className="cliptwo">New Album</h3><h1 className="clip">Medley Attack!!!</h1><h3 className="cliptwo">July 08 2022</h3>
+        </a> 
+        <a href="https://spiralstairspsoi.bandcamp.com/" className="cliptwo mt-5 h3" >PreOrder <img className="preorder" src="images/ElBuen.png" width="40%" alt="el buen" /> Now</a>
         </Row>
         <Row className="justify-content-center mt-5" flex="true">
             <Col xs={3} flex="true">
@@ -19,7 +21,6 @@ export default function Home(){
         <Col xs={6} flex="true" style={{display: "grid", textAlign: "center", color: "white", position: "relative" }}>
             <a href="https://spiralstairspsoi.bandcamp.com/">
             <img src="/images/MedleyAttack_FrontCover.jpg" alt="Medley Attack" className="img-fluid" style={{borderImage: 'url(/images/Rainbow.jpg) 100', borderWidth: '5px', borderStyle: 'solid', width: '100%', align: 'center'}} />
-            <div className="imagetext">Pre-Order Now</div>
             </a>
         </Col>
         <Col xs={3} flex="true">
