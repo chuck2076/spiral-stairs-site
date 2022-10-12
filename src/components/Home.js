5053f22241fcfc4b4a82d8028c20cd95ca016f3f
Chuck Stephens <chuckstephens2076@gmail.com>
2022-10-12 14:13:43 -0500

    some css changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,22 +2,23 @@
 
 import React from "react";
 import {Container, Row, Col} from 'react-bootstrap'
+import {Link} from 'react-router-dom'
 
 
 export default function Home(){
     return(
     <Container flex="true">
-        <Row className="justify-content-center mt-5">
-            <Col xs={6} md={6}>
-        <a flex="true" href="https://spiralstairspsoi.bandcamp.com/">
+        <Row className="mt-5">
+            <Col xs={6} md={6} className="justify-space-between">
+        <Link flex="true" to="https://spiralstairspsoi.bandcamp.com/">
         <div className="cliptwo">New Album</div><h2 className="clip">Medley Attack!!!</h2><div className="cliptwo">July 08 2022</div>
-        </a> 
-            </Col>
-            <Col xs={6} md={6} className="justify-content-center align-items-center">
-        <a href="https://spiralstairspsoi.bandcamp.com/" className="cliptwo ml-5">Order Now<br></br> <img className="preorder" src="images/ElBuen.png" width="25%" alt="el buen" /></a>
+        </Link>
+        </Col>
+            <Col xs={6} md={6} className="justify-space-between">
+        <Link to="https://spiralstairspsoi.bandcamp.com/" className="cliptwo ml-5">Order Now<img className="preorder" src="images/ElBuen.png" width="25%" alt="el buen" /></Link>
         </Col>
         </Row>
-        <Row className="justify-content-center mt-3" flex="true">
+        <Row className= "mt-3" flex="true">
             <Col xs={3} flex="true">
                 <img src='images/Deer.png' alt="Deer" style={{width: '80%'}} className="img-fluid" />
         </Col>
